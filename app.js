86c3890b08439e1f62b9e01faea4af2e021e89e8
William Jagels <will.jagels@gmail.com>
2015-09-19 03:48:36 -0400

    Generating access tokens from auth codes

--- app.js
+++ app.js
@@ -37,10 +37,10 @@ app.use(function(req, res, next) {
 if (app.get('env') === 'development') {
   app.use(function(err, req, res, next) {
     res.status(err.status || 500);
-    res.render('error', {
+    /*res.render('error', {
       message: err.message,
       error: err
-    });
+    });*/
   });
 }
 
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,33 @@
 var express = require('express');
 var router = express.Router();
 
+var aws = require('aws-sdk');
+var Uber = require('node-uber');
+
+var uber = new Uber({
+  client_id: process.env.UBER_CID,
+  client_secret: process.env.UBER_CS,
+  server_token: process.env.UBER_ST,
+  redirect_uri: 'http://localhost:3000/oauth',
+  name: 'ÜberQ'
+});
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+router.get('/check', function(req, res, next) {
+
+});
+
+router.get('/uberauth', function(req, res, next) {
+    console.log(req.query.rt);
+    uber.authorization({authorization_code: req.query.rt}, function(err, access_token, refresh_token) {
+        if(err) console.error(err);
+        console.log(access_token);
+        console.log(refresh_token);
+    });
 });
 
 module.exports = router;
