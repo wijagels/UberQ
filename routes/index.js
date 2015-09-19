b22e5f1323285fe9bbd72b57b0d65a0115d7de02
William Jagels <will.jagels@gmail.com>
2015-09-19 14:47:17 -0400

    Adding to databaseeeeee

--- routes/index.js
+++ routes/index.js
@@ -57,8 +57,23 @@ router.get('/uberauth', function(req, res, next) {
                 'refresh_token': refresh_token
             });
             var params = {
+                Item: {
+                    gcm_id: {
+                        S: req.query.gcm_id
+                    },
+                    uber_access: {
+                        S: access_token
+                    },
+                    uber_refresh: {
+                        S: refresh_token
+                    },
+                },
+                TableName: 'Users'
             };
-        }
+            dynamodb.putItem(params, function(err, data) {
+                if (err) console.log(err, err.stack); // an error occurred
+                else     console.log(data);           // successful response
+            }); }
     });
 });
 
